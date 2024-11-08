707397367ecc0586271e1020c41838368de2d3d0
bcherng <briancherngsch@gmail.com>
2024-11-08 09:46:26 -0800

    Reploy Questionnaire module using serverless function instead of Express

--- api/questionnaire.js
+++ api/questionnaire.js
@@ -1,27 +1,41 @@
 require('dotenv').config({path: "../.env"});
-const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../models/users');
 
-const app = express();
-app.use(express.json());
-
 // Connect to MongoDB
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
-// Submit questionnaire
-app.post('/api/questionnaire', async (req, res) => {
-    const { questionnaire, userId } = req.body;
 
-    try {
-        await User.findByIdAndUpdate(userId, { questionnaire });
-        res.json({ message: 'Questionnaire submitted successfully' });
-    } catch (error) {
-        res.status(500).json({ message: 'Error submitting questionnaire' });
+module.exports = async (req, res) => {
+    if (req.method === 'POST') {
+        try {
+            const { questionnaire, userId } = req.body;
+            try {
+                await User.findByIdAndUpdate(userId, { questionnaire });
+                res.json({ message: 'Questionnaire submitted successfully' });
+            } catch (error) {
+                res.status(500).json({ message: 'Error submitting questionnaire' });
+            }
+        } catch (error) {
+            res.status(500).json({ message: 'Error submitting questionnaire', error });
+        }
+    } else if (req.method === 'GET') {
+        const { userId } = req.query; 
+        try {
+            const user = await User.findById(userId);
+            if (user) {
+                res.status(200).json({ questionnaire: user.questionnaire });
+            } else {
+                res.status(404).json({ message: 'User not found' });
+            }
+        } catch (error) {
+        res.status(500).json({ message: 'Error retrieving questionnaire', error });
+        }
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
     }
-});
-
-module.exports = app;
+};
