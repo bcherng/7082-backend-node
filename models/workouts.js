10d0675244ac0fdf67306743b610f86ffe0f9448
bcherng <briancherngsch@gmail.com>
2024-11-08 10:09:01 -0800

    Modify schema to username instead of userId, to differentiate from Mongo's _id

--- models/workouts.js
+++ models/workouts.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const workoutSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    routine: { type: Object, required: true }  // Store workout routine by day
+    username: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    routine: { type: Object, required: true }  
 });
 
 module.exports = mongoose.model('Workout', workoutSchema);
