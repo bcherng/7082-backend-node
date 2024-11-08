882fc69cf777f4b4b6db1dd636a42de6c8685cc2
bcherng <briancherngsch@gmail.com>
2024-11-08 10:08:34 -0800

    Modify schema to username instead of userId, to differentiate from Mongo's _id

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    questionnaire: { type: Object } // Store questionnaire responses here
+    questionnaire: { type: Object }
 });
 
 module.exports = mongoose.model('User', userSchema);
