1697028bf324dcdf171359d2ed87b0ea547ac5bb
bcherng <briancherngsch@gmail.com>
2024-11-08 09:39:37 -0800

    Reploy using serverless function instead of Express

--- server.js
+++ server.js
@@ -29,31 +29,7 @@ function authenticateToken(req, res, next) {
     });
 }
 
-// Signup
-app.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        const newUser = await User.create({ username, password: hashedPassword });
-        res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-        res.status(400).json({ message: 'User already exists' });
-    }
-});
-
-// Login
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
-});
 
 // Submit questionnaire
 app.post('/questionnaire', authenticateToken, async (req, res) => {
