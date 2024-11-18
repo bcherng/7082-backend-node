e9f17344d690798a3b07b69dd7ded22282a1feed
ZhengHanShuu <hjacksonhjh@gmail.com>
2024-11-17 17:27:46 -0800

    updated the cors header

--- api/signup.js
+++ api/signup.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 module.exports = async (req, res) => {
 
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
