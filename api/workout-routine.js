fcc5abc2897c73263b1f0b00d8c28d841b69f77a
ZhengHanShuu <hjacksonhjh@gmail.com>
2024-11-21 20:54:01 -0800

    added cors headers

--- api/workout-routine.js
+++ api/workout-routine.js
@@ -11,6 +11,13 @@ mongoose.connect(process.env.MONGODB_URI, {
     TO-DO: verify user before allowing access
 */
 module.exports = async (req, res) => {
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     if (req.method === 'GET') {
         const username = req.query.username;
         const { day } = req.query;
