c4547b716f0e62d40604d5e0c038322e889733b2
ZhengHanShuu <hjacksonhjh@gmail.com>
2024-11-21 19:21:00 -0800

    added cors headers

--- api/questionnaire.js
+++ api/questionnaire.js
@@ -10,6 +10,13 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 module.exports = async (req, res) => {
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
     if (req.method === 'POST') {
         const username = req.body.username;
         const questionnaire = req.body.questionnaire || null;
